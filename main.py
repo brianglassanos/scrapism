dda3b897962697932a79315ecf31e9afeda754f6
brianglassanos <brglassan@gmail.com>
2020-09-09 22:54:13 -0700

    Added about page

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def title_one():
+    return 'SCRAPISM v1.0'
+
+@app.route('/about')
+def about():
+    return 'Scrapism is a BS4 web scraper for finding business website contact/support information.'
